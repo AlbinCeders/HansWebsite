f8c8179526d0e0c527bb998c7067a5e0acdb854c
AlbinCeders <albinceders@gmail.com>
2026-08-19 16:47:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.querySelectorAll('.project-hover-video').forEach((video) => {
+  const project = video.closest('.project');
+
+  project.addEventListener('mouseenter', () => {
+    video.play().catch(() => {});
+  });
+
+  project.addEventListener('mouseleave', () => {
+    video.pause();
+    video.currentTime = 0;
+  });
+});
